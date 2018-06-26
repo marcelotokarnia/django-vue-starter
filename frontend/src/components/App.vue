e76e5480d039570e3f94586c5a764bfa7074d2cd
Marcelo Tokarnia <marcelo.tokarnia@vtex.com.br>
2018-06-25 21:59:42 -0300

    initial commit

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar />
+    <Main />
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Main from '@src/components/Main.vue'
+  import Navbar from '@src/components/Navbar.vue'
+  export default Vue.extend({
+    name: 'App',
+    components: {
+      Main,
+      Navbar
+    }
+  })
+</script>
+
+<style>
+</style>
---
+++ frontend/src/index.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from '@src/components/App.vue'
+
+const v = new Vue({
+    components: {
+        App,
+    },
+    el: '#app',
+    template: `<App />`,
+})
